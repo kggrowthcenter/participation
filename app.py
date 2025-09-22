0841624d9aba7c4ae28479ccb6685969a50203ba
kggrowthcenter <data@growthcenter.id>
2025-09-22 15:54:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -339,6 +339,7 @@ if st.session_state['authentication_status']:
         "HOTELS & RESORTS": "GOHR",
         "G. RETAIL & PUBLISHING": "GORP",
         "RETAIL & PUBLISHING": "GORP",
+        "G. MANUFACTURE" : "GOMAN"
     }
     df_concise['unit'] = df_concise['unit'].replace(rename_units)
     df_concise['subunit'] = df_concise['subunit'].apply(extract_main_unit)
