d07d0eca23b01a2e59946f515ba6492ad48a4fb1
kggrowthcenter <data@growthcenter.id>
2025-10-07 15:11:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -291,6 +291,7 @@ if st.session_state['authentication_status']:
     df_concise = df_concise.loc[df_concise['unit'] != 'KG'].reset_index(drop=True)
 
     df_concise = df_concise.loc[df_concise['nik'] != '012172'].reset_index(drop=True)
+    df_concise = df_concise.loc[df_concise['nik'] != '091412'].reset_index(drop=True)
 
     # Drop rows where the position is 'Store Apprentice'
     df_concise = df_concise.loc[df_concise['position'] != 'Store Apprentice'].reset_index(drop=True)
